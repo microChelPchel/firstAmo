914b06c70fe27bc8d72efae1c4b31491cce75ee2
microChelPchel <108371410+microChelPchel@users.noreply.github.com>
2023-05-13 18:03:24 +0300

    Amo-4 учитывать дни в году в наборах данных (#3)

--- model_preprocessing.py
+++ model_preprocessing.py
@@ -3,25 +3,20 @@ import numpy as np
 import os
 
 def data_preprocessing(path):
-    # Для каждой папки (train и test) выполняем предобработку
     for folder_name in ['train', 'test']:
-        # Считываем все файлы из папки
         folder_path = os.path.join(path, folder_name)
         file_names = os.listdir(folder_path)
-        # Для каждого файла выполняем предобработку
         for file_name in file_names:
-            # Считываем данные из файла и переводим в numpy-массив
             file_path = os.path.join(folder_path, file_name)
             data = np.loadtxt(file_path, delimiter=',', dtype=None)
-            data = data.reshape(-1, 1)   # Изменяем размерность массива
-            # Стандартизируем данные с помощью StandardScaler
+            X = data[:, :-1] # выбираем все столбцы, кроме последнего
+            y = data[:, -1] # выбираем последний столбец
             scaler = StandardScaler()
-            data = scaler.fit_transform(data)
-            # Сохраняем предобработанные данные в файл с именем 'original_name_scaled.txt'
+            X_scaled = scaler.fit_transform(X)
+            data_scaled = np.hstack((X_scaled, y.reshape(-1, 1))) # объединяем масштабированные данные и целевую переменную
             new_file_path = os.path.join(folder_path, file_name[:-4] + '_scaled.txt')
-            np.savetxt(new_file_path, data, delimiter=',')
+            np.savetxt(new_file_path, data_scaled, delimiter=',')
 
 if __name__ == '__main__':
-    # Путь к папке с тренировочными и тестовыми данными
     path = '.'
     data_preprocessing(path)
