f60025e054021f48e7a8871a9abe47f9e08c54a2
microChelPchel <108371410+microChelPchel@users.noreply.github.com>
2023-05-13 18:03:02 +0300

    Amo-4 учитывать дни в году в наборах данных

--- data_creation.py
+++ data_creation.py
@@ -16,7 +16,7 @@ def generate_data(start_temp, num_days, anomaly=None, noise=None):
             temp += anomaly[1]
         if noise:
             temp += random.uniform(-noise, noise)
-        data.append(temp)
+        data.append([i+1, round(temp, 2)])
     return data
 
 # Создаем n наборов данных для обучения модели
@@ -24,7 +24,7 @@ def create_train_data(count):
     for i in range(count):
         data = generate_data(20, 365, anomaly=(50, -5), noise=2)
         with open(f'train/data_{i}.txt', 'w') as f:
-            f.writelines(str(d) + '\n' for d in data)
+            f.writelines(str(d[0]) + ',' + str(d[1]) + '\n' for d in data)
 
 
 # Создаем n наборов данных для тестирования модели
@@ -32,7 +32,7 @@ def create_test_data(count):
     for i in range(count):
         data = generate_data(20, 365, anomaly=(100, 10), noise=3)
         with open(f'test/data_{i}.txt', 'w') as f:
-            f.writelines(str(d) + '\n' for d in data)
+            f.writelines(str(d[0]) + ',' + str(d[1]) + '\n' for d in data)
 
 if __name__ == '__main__':
     create_train_data(5)
