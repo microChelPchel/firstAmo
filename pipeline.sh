2e3d808809866667092dcb5d3c416eee80dea649
microChelPchel <108371410+microChelPchel@users.noreply.github.com>
2023-05-21 11:35:45 +0300

    Amo-8 добавлен Readme файл

--- pipeline.sh
+++ pipeline.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
 echo 'запуск скрипта создания данных'
 python3 data_creation.py
-
 echo 'запуск скрипта обработки и стандартизации данных'
 python3 model_preprocessing.py
-
 echo 'запуск скрипта создания модели и обучения'
 python3 model_preparation.py
-
 echo 'запуск скрипта создания модели и обучения'
 python3 model_testing.py
